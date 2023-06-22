0ccb56ac7df909151bb7c6ea46f277c738656333
AbsorbedPoem <alejandrovgz0804@gmail.com>
2023-06-22 17:42:55 -0400

    domcontentloaded

--- vista/js/login.js
+++ vista/js/login.js
@@ -1,1 +1,3 @@
-alert("=)(W=D)AS(=D)IA=)I=)(=Q)DQWU=D)UDASJODIJAOIJDOKOQIJ#RO#ERLQKWMDLAKSN... se bugueó");
+document.addEventListener("DOMContentLoaded", (event) => {
+    alert("=)(W=D)AS(=D)IA=)I=)(=Q)DQWU=D)UDASJODIJAOIJDOKOQIJ#RO#ERLQKWMDLAKSN... se bugueó");
+});
