acfa5ec8528eeed06ebd6cf56e740f5eba640089
AbsorbedPoem <alejandrovgz0804@gmail.com>
2023-07-09 19:35:31 -0400

    css de tabla

--- vista/modulos/main/dashboard.php
+++ vista/modulos/main/dashboard.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="vista/css/tabla.css">
         <title>D</title>
         <style>
             body {
@@ -46,18 +47,18 @@
         <div class="container-fluid p-5">
             <h3 class="ps-5">Panorama general</h3>
             <table class='table custom-table mt-4'>
-            <thead class='headTableForQuote'>
-                <tr>
-                    <td class='fw-bold' style='background-color: #21a52c; color: white;'>código</td>
-                    <td class='fw-bold' style='background-color: #21a52c; color: white;'>animal</td>
-                    <td class='fw-bold' style='background-color: #21a52c; color: white;'>edad</td>
-                    <td class='fw-bold' style='background-color: #21a52c; color: white;'>peso</td>
-                    <td class='fw-bold' style='background-color: #21a52c; color: white;'>estado</td>
-                </tr>
-            </thead>
-            <tbody>
+                <thead class='headTableForQuote'>
+                    <tr>
+                        <td class='fw-bold' style='background-color: #21a52c; color: white;'>código</td>
+                        <td class='fw-bold' style='background-color: #21a52c; color: white;'>animal</td>
+                        <td class='fw-bold' style='background-color: #21a52c; color: white;'>edad</td>
+                        <td class='fw-bold' style='background-color: #21a52c; color: white;'>peso</td>
+                        <td class='fw-bold' style='background-color: #21a52c; color: white;'>estado</td>
+                    </tr>
+                </thead>
+                <tbody>
 
-            </tbody>
+                </tbody>
         </table>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
