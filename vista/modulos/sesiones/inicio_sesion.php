23e4db4cc098419d1bbfe1ed59448cd0b2b45aaf
gabrielbello19 <tudirecciondecorreo@example.com>
2023-07-09 17:08:29 -0400

    x

--- vista/modulos/sesiones/inicio_sesion.php
+++ vista/modulos/sesiones/inicio_sesion.php
@@ -10,17 +10,18 @@
     <body>
         <div class="contenedor">
             <form>
-                <h1>Iniciar Sesión</h1>
-                <input class="input-usuario" type="text" name="usuario" placeholder="Usuario">
-                <input class="input-usuario" type="password" name="contraseña" placeholder="Contraseña">
+                <h1>Iniciar Sesión.</h1>
+                <input class="input-usuario" type="text" name="usuario" placeholder="Usuario.">
+                <input class="input-usuario" type="password" name="contraseña" placeholder="Contraseña.">
                 <input class="boton-enviar" type="submit" value="Entrar">
             </form>
-            <a class="enlace" href="vista/modulos/sesiones/registrarse.html">>> o regístrate</a>
+            <a class="enlace" href="vista/modulos/sesiones/registrarse.php">> No estas registrado?, regístrate.</a>
         </div>
+        
         <footer>
-            <h4>Nuestra ubicación</h4>
-            <p>Turmero, Edo. Aragua, Venezuela</p>
-            <h4>Contacto</h4>
+            <h4>Nuestra ubicación:</h4>
+            <p>Turmero, Edo. Aragua, Venezuela.</p>
+            <h4>Contacto:</h4>
             <p>Teléfono: 0424-3655004</p>
             <p>Correo electrónico: gaboxbellox@gmail.com</p>
        </footer>
--- vista/modulos/sesiones/registrarse.php
+++ vista/modulos/sesiones/registrarse.php
@@ -9,32 +9,24 @@
     <body>
         <div class="contenedor">
          <form action="" method="post">
-               <h1>Registrate</h1>
-               <input class="input-usuario" type="number" name="cedula" placeholder="Cedula">
-               <input class="input-usuario" type="text" name="n_usuario" placeholder="Nombre de usuario">
-               <input class="input-usuario" type="text" name="nombre" placeholder="Nombre">
-               <input class="input-usuario" type="text" name="apellido" placeholder="Apellido">
-               <input class="input-usuario" type="text" name="correo" placeholder="Correo">
-               <input class="input-usuario" type="password" name="contraseña" placeholder="Contraseña">
-               <input class="input-usuario" type="date" name="fecha_n" placeholder="Fecha de nacimiento">
+               <h1>Registrate.</h1>
+               <input class="input-usuario" type="number" name="cedula" placeholder="Cedula.">
+               <input class="input-usuario" type="text" name="n_usuario" placeholder="Nombre de usuario.">
+               <input class="input-usuario" type="text" name="nombre" placeholder="Nombre.">
+               <input class="input-usuario" type="text" name="apellido" placeholder="Apellido.">
+               <input class="input-usuario" type="text" name="correo" placeholder="Correo.">
+               <input class="input-usuario" type="password" name="contraseña" placeholder="Contraseña.">
+               <input class="input-usuario" type="date" name="fecha_n" placeholder="Fecha de nacimiento.">
                <input class="boton-enviar" type="submit" value="Registrar">
          </form>
         </div>
 
         <footer>
-	      <div class="container">
-		        <div class="row">
-		    	    <div class="col-md-6">
-		    		    <h4>Nuestra ubicación</h4>
-		    		    <p>Turmero, Edo. Aragua, Venezuela</p>
-		    	    </div>
-		    	    <div class="col-md-6">
-		    		    <h4>Contacto</h4>
-		    		    <p>Teléfono: 0424-3655004</p>
-		    		    <p>Correo electrónico: gaboxbellox@gmail.com</p>
-		    	    </div>
-	    	    </div>
-	        </div>
+            <h4>Nuestra ubicación:</h4>
+            <p>Turmero, Edo. Aragua, Venezuela.</p>
+            <h4>Contacto:</h4>
+            <p>Teléfono: 0424-3655004</p>
+            <p>Correo electrónico: gaboxbellox@gmail.com</p>
        </footer>
     </body>
 </html>
